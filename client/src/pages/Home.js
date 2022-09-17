e81d288d0ae386e289c722f64e012bd15bfced4e
vhivestate <garcia.valeria001@gmail.com>
2022-09-16 20:06:03 -0600

    about

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='about'>
+      <div>
+      <h1>ABOUT US</h1>
+      <p>Blank Space is a bridge between the world of tattooing and the walls of a gallery. We aim to merrge these to industries to form a create space that is both thought provoking for the public as well as, theraputic for the clientel. When brought together they fill the black space.</p>
+      </div>
+    </div>
   )
 }
 
