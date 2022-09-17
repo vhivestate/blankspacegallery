257f9f3ee7eb4090fedb32dde61790225bd5d88c
vhivestate <garcia.valeria001@gmail.com>
2022-09-16 20:47:48 -0600

    footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,11 +2,25 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer>
+    <footer className="footer p-10 bg-base-200 text-base-content">
+        <div className="form-control w-80">
+            <span className="footer-title">Newsletter</span> 
+            <label className="label">
+                <span className="label-text">Enter your email address</span>
+            </label> 
+        <div className="relative">
+            <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" /> 
+            <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">Subscribe</button>
+          </div>
+        </div>
+
         <div>
-            <p>LOCATION</p>
+          <span className="footer-title">LOCATION</span> 
             <p>298 24th Street, Ogden, UT</p>
-            <p>CONTACT</p>
+        </div>
+
+        <div>
+          <span className="footer-title">CONTACT</span> 
             <p>blankspacegalleryllc@gmail.com</p>
         </div>
     </footer>
--- client/src/pages/Artist.js
+++ client/src/pages/Artist.js
@@ -1,10 +1,27 @@
 
 
 import React from 'react'
+import Garrett from '../assets/avatar.jpg'
+import David from '../assets/avatar2.jpg'
 
 function Artist() {
   return (
-    <div>Artist</div>
+    <div className='artists'>
+     <h1>ARTISTS</h1>
+      <div className='artist1'>
+        <h2>Garrett Smith</h2>
+        <div className='Avatar'>
+            <img src={Garrett}></img>
+        </div>
+      </div>
+
+      <div className='artist2'>
+        <h2>David Carter</h2>
+        <div className='Avatar2'>
+            <img src={David}></img>
+        </div>
+      </div>
+    </div>
   )
 }
 
