6ab6b48201de6dc69b3eb12ba57dbdb9da834ed3
vhivestate <garcia.valeria001@gmail.com>
2022-09-23 20:54:19 -0600

    installed router

--- client/src/pages/Artist.js
+++ client/src/pages/Artist.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import Garrett from '../assets/avatar.jpg'
 import David from '../assets/avatar2.jpg'
-import { Instagram } from 'react-feather';
-
 
 function Artist() {
   return (
@@ -12,8 +10,8 @@ function Artist() {
          <div className="card w-96 glass">
       <figure><img src={Garrett} alt="car!"/></figure>
       <div className="card-body">
-        <h2 className="card-title">GARRETT SMITH</h2>
-      <div className="card-actions justify-end">
+        <h2 className="card-title justify-center">GARRETT SMITH</h2>
+      <div className="card-actions justify-center">
       <button className="btn btn-primary">View Portfolio</button>
     </div>
   </div>
@@ -24,8 +22,8 @@ function Artist() {
 <div className="card w-96 glass">
   <figure><img src={David} alt="car!"/></figure>
   <div className="card-body">
-    <h2 className="card-title">DAVID</h2>
-    <div className="card-actions justify-end">
+    <h2 className="card-title justify-center">DAVID CARTER</h2>
+    <div className="card-actions justify-center">
               <button className="btn btn-primary">View Portfolio</button>
              </div>
              </div>
