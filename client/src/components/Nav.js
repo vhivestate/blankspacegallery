88456b83ad9c615c8d3bce8b6d5b4cbd84d5f30e
vhivestate <garcia.valeria001@gmail.com>
2022-10-25 22:38:06 -0600

    nav bg color

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -10,7 +10,7 @@ function Nav() {
             <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>            </label>
-            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-black rounded-box w-52">
             <li><Link to={'/artists'}>Artists</Link></li>
             {/* <li><Link to={'/booking'}>Booking</Link></li> */}
             <li><Link to={'/gallery'}>Gallery Exhibit</Link></li>
